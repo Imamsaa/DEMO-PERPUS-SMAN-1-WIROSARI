36fb8e355d6634a5550d59fba9daa9b8ee08b570
Imamsaa <imamsafii2222022@gmai.com>
2023-10-10 22:12:09 +0700

    Mau Menambahkan Database

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,6 @@ class ComposerAutoloaderInita9337121b4af695c0cdac9677ef40fa1
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInita9337121b4af695c0cdac9677ef40fa1::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }
